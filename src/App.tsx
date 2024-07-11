0f5c0986238a543027ce6d076b1937e823381177
Azim Sofi <azimmiskandar@gmail.com>
2024-07-11 18:30:07 +0900

    コメント

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; // フック
 import "./App.css";
 
 function App() {
+  // 定義
+  // [変数, setter()] = 初期化
   const [inputText, setInputText] = useState("");
-  //todo配列オブジェクトの更新用に用意。プロパティはinputValue, id, checkedの３つを更新する。
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  // クラスみたいな感じ
   type Todo = {
     inputValue: string;
-    id: number; //keyを指定するため
+    id: number;
     checked: boolean;
   };
 
+  // handleChangeは変数、関数を代入する
+  // 「e」は関数の引数、React.ChangeEvent<HTMLInputElement>形、target.valueを使うように
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    // preventDefault()はJavascriptのイベントオブジェクト「e」のメソッド
+    // 動作をキャンセルする
     e.preventDefault();
 
+    // e.target = イベントが発生した要素 = <input>
+    // .value = 要素の現在の値を取得する = ユーザの入力
     setInputText(e.target.value);
   };
 
+  // preventDefault: () => voidはjavascriptの一般的なオブジェクト型
+  //　それとも e: React.FormEvent<HTMLFormElement>　もOK
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
@@ -27,19 +38,19 @@ function App() {
 
     //新しいTodo作成
     const newTodo: Todo = {
-      inputValue: inputText,
-      id: todos.length,
-      checked: false,
+      inputValue: inputText, // className="inputText"
+      id: todos.length, // todos変数の配列のサイズ
+      checked: false, 
     };
 
-    setTodos([newTodo, ...todos]);
+    setTodos([newTodo, ...todos]); // 配列・追加
     console.log(inputText);
     setInputText(inputText);
   };
 
   //todo編集
-  const handleEdit = (id: number, inputValue: string) => {
-    /* ディープコピー(完全にコピーされた別の配列)に変えよう(ミューテートから守るため) */
+  const handleEdit = (id: number, inputValue: string) => { // id, inputValue = 引数
+    // 元の配列をミューテート（直接変更）するのを防ぐため
     const deepCopy = todos.map((todo) => ({ ...todo }));
     console.log(deepCopy);
 
@@ -50,6 +61,7 @@ function App() {
       return todo;
     });
 
+    // 元のメモリを共有する
     // /* シャローコピー */
     // const newTodos = todos.map((todo) => {
     //   if (todo.id === id) {
